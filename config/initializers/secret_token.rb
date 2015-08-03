735d643a39581fa9280669e236276b56cf1f1794
Natsuki Ito <sf.fm.forza@outlook.com>
2015-08-04 03:00:26 +0900

    Inital commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WcgServer::Application.config.secret_key_base = 'fcea16300b7fd2ef24f713013f8a2427989ec4b75d7c69c41fa96a7ff92c366095c9c05f6fe101556e5fdc1c60af8d226b28267f2ac3bfa4560bb225fb97985f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WcgServer::Application.config.session_store :cookie_store, key: '_wcg-server_session'
